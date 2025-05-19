ec75b88c27f28a73dd7f9e3a8e8bb468d69fb89e
ArnaudM1983 <arnaudmeusy@hotmail.com>
2025-05-19 17:02:51 +0200

    Initial commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,25 @@
+<template>
+    <header>
+        <div class="flex justify-between items-center p-8 lg:px-12 relative z-20">
+            <div class="text-3xl text-red-500">LOGO</div>
+
+            <!-- Mobile Toggle Button-->
+             <div class="md:hidden z-30">
+                <button class="block focus:outline-none" @click="isMenuOpen = !isMenuOpen">
+                    <span v-if="isMenuOpen" class="text-5xl text-red-500">
+                        <Icon icon="material-symbols:close"/>
+                    </span>
+                    <span v-else class="text-5xl text-red-500">
+                        <Icon icon="material-symbols:menu"/>
+                    </span>
+                </button>
+             </div>
+
+        </div>
+    </header>
+</template>
+<script setup>
+import { ref } from "vue";
+const isMenuOpen = ref(false);
+
+</script>

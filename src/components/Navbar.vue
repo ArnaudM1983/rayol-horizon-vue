64a3fdd23899c88d041c31a7951bbebf34a4e8e0
ArnaudM1983 <arnaudmeusy@hotmail.com>
2025-05-21 21:11:04 +0200

    Maj menu burger (modif background + center)

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,22 +1,27 @@
 <template>
-  <header class="absolute top-0 mt-2 left-0 w-full z-50 bg-transparent text-white">
+  <header class="absolute top-0 left-0 w-full z-50 bg-transparent text-white">
 
     <div class="flex justify-between items-center p-6 lg:px-12">
 
       <!-- Mobile Toggle Button -->
-      <div class="md:hidden z-30">
-        <button @click="isMenuOpen = !isMenuOpen" class="text-3xl text-red-500">
+      <div class="md:hidden fixed top-4 left-1/2 transform -translate-x-1/2 z-50">
+        <button @click="isMenuOpen = !isMenuOpen"
+          class="w-14 h-14 flex items-center justify-center rounded-full bg-black/10 backdrop-blur-md text-black text-4xl shadow-lg">
           <Icon :icon="isMenuOpen ? 'material-symbols:close' : 'material-symbols:menu'" />
         </button>
       </div>
 
+
       <!-- Nav Links -->
       <nav :class="[
-        'absolute md:static left-0 top-full w-full md:w-auto bg-white md:bg-transparent p-6 md:p-0 shadow-md md:shadow-none',
-        'flex flex-col md:flex-row items-center gap-6 md:gap-16',
-        isMenuOpen ? 'flex' : 'hidden',
-        'md:flex'
+        'left-1/2 w-[280px] p-6 flex flex-col items-center gap-6 shadow-md md:flex-row md:gap-16 md:p-0',
+        isMenuOpen ? 'flex fixed top-[90px] -translate-x-1/2 bg-black/20 backdrop-blur-md rounded-lg' : 'hidden',
+        'md:static md:w-auto md:bg-transparent md:backdrop-blur-0 md:shadow-none md:flex md:translate-x-0 md:left-auto md:max-w-full md:rounded-none'
       ]">
+
+
+
+
         <a href="#lappartement" @click.prevent="scrollToSection('#lappartement')"
           class="relative text-white font-light uppercase text-xl after:content-[''] after:absolute after:left-0 after:-bottom-1 after:w-0 after:h-[1px] after:bg-white after:transition-all after:duration-300 hover:after:w-full">L'Appartement</a>
         <a href="#equipements" @click.prevent="scrollToSection('#equipements')"
@@ -30,9 +35,10 @@
 
       <!-- Réserver Button -->
       <a href="#contact"
-        class="text-white border border-white px-6 py-2 text-lg font-light cursor-pointer bg-transparent rounded-none hover:bg-white hover:text-black transition">
+        class="hidden md:inline-block text-white border border-white px-6 py-2 text-lg font-light cursor-pointer bg-transparent rounded-none hover:bg-white hover:text-black transition">
         Je réserve
       </a>
+
     </div>
   </header>
 </template>
